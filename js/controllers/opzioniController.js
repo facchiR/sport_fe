68916e9366e29a5f052fcb776494afc3e3998eac
Valerio Sanguineti <valerio.sanguineti@villaggio.org>
2016-11-14 12:22:57 +0100

    emails

--- js/controllers/opzioniController.js
+++ js/controllers/opzioniController.js
@@ -2,13 +2,11 @@ app.controller('OpzioniController', ['$scope', 'crudService','$routeParams','$ht
 	var vm = $scope;
 	window.vm=vm;
 	vm.cat=$routeParams && $routeParams.opz || 'classi';
-	vm.id= false;
     vm.data = [];
 	vm.gridOptions={
 		enableCellEditOnFocus: true,
 		enableRowSelection: true,
 		selectionRowHeaderWidth: 35,
-		rowHeight: 35,		
 		enableSelectAll: false,
 		multiSelect:false,
 		onRegisterApi:function(gridApi){
@@ -16,16 +14,16 @@ app.controller('OpzioniController', ['$scope', 'crudService','$routeParams','$ht
 			gridApi.rowEdit.on.saveRow(vm, vm.save);
 		}		
 	}
-    var populateData = function(response){
+    vm.populateData = function(response){
         vm.data = response.data && response.data.docs ||[];
 		vm.gridOptions.data=vm.data
 		
 		switch (true){
 			case (vm.cat=='persone'):
 				vm.gridOptions.columnDefs=[
-					{ name: 'cognome', displayName: 'Cognome' },
+					{ name: 'cognome'},
 					{ name: 'nome' }
-				];
+				]; 
 				break;
 			case (vm.cat=='classi'):
 				vm.gridOptions.columnDefs=[
@@ -50,19 +48,12 @@ app.controller('OpzioniController', ['$scope', 'crudService','$routeParams','$ht
 		}
 		
     };
-	vm.addRow=function(){
+	vm.create=function(){
 		vm.data.push({_id:null})
-	};
-	vm.deleteRow=function(row,grid) {
-		if (!grid) return 
-		if (!grid.options) return
-	   var i = grid.options.data.indexOf(row.entity);
-	   grid.options.data.splice(i, 1);
 	};
     vm.read = function(){
 		var fnd={"cat":vm.cat};
-		if (vm.id) fnd._id=vm.id;
-        crudService.fnd(fnd, populateData);
+        crudService.fnd(fnd, vm.populateData);
     };	
     vm.save = function(row){
 		var promise = $q.defer();
@@ -74,18 +65,17 @@ app.controller('OpzioniController', ['$scope', 'crudService','$routeParams','$ht
     };
 	vm.remove = function(){	
 		var d=vm.gridApi.grid.selection.lastSelectedRow.entity
-		vm.data.splice(vm.data.indexOf(d),1)	
+		vm.data.splice(vm.data.indexOf(d),1)
         crudService.del(d,function(r){
 			//window.location="#/"+vm.cat+"/"
-			
 		});
     };
     vm.init = function(){
         $(function(){
-			var gc=$('.ui-grid')
+			var gc=$('.ui-grid');
 			var po=gc.position();
 			var gh=window.innerHeight-po.top-100;
-			if (gh<150) gh=150;			
+			if (gh<150) gh=150;
 			gc.height(gh);
 		})
         vm.read();
