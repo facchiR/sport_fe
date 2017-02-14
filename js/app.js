aae087b56984c67ced6d035b343356c645af07b7
Fabio <Fabio>
2017-02-14 07:29:07 +0100

    Primo commit

--- js/app.js
+++ js/app.js
@@ -1,30 +1,24 @@
-var app = angular.module('ngschool', ['ngRoute','ui.grid','ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav','ui.grid.selection']);
-app.conf=function ($routeProvider) { 
+var app = angular.module('sport', 
+    ['ngRoute']);
+
+app.config(function ($routeProvider) { 
   $routeProvider 
     .when('/', { 
       templateUrl: 'views/home.html' 
     })
-    .when('/persone', { 
-      controller: 'PersoneController', 
-      templateUrl: 'views/persone.html' 
+    .when('/atleti', { 
+      controller: 'AtletiController', 
+      templateUrl: 'views/atleti.html' 
     })
-    .when('/persone/:id', { 
-      controller: 'PersoneController', 
-      templateUrl: 'views/persone.html' 
+    .when('/atleti/:id', { 
+      controller: 'AtletiController', 
+      templateUrl: 'views/atleta.html' 
     }) 
-    .when('/opzioni', { 
-      controller: 'OpzioniController', 
-      templateUrl: 'views/opzioni.html' 
-    })
-    .when('/opzioni/:opz', { 
-      controller: 'OpzioniController', 
-      templateUrl: 'views/opzioni.html' 
-    })	
     .otherwise({ 
       redirectTo: '/' 
     }); 
-}
-app.config(app.conf);
+});
+
 app.config(['$httpProvider', function($httpProvider) {
     if (!$httpProvider.defaults.headers.get) {
         $httpProvider.defaults.headers.get = {};    
@@ -33,6 +27,8 @@ app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
     $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 }]);
-app.constant("swInfo", {
-    "version": "1.0.1"
+
+app.constant("settings", {
+    "ver": "1.0.0",
+    "url": "http://localhost:88/sport_be/",
 });
--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -1,25 +1,12 @@
-app.controller('AppController', ['swInfo', function( swInfo ) {
+app.controller('AppController', function() {
     var vm = this;
     vm.nav = [
        {
         url:"#/",
         title:"Home"
        },{
-        url:"#/persone",
-        title:"Persone"
-       },{
-        url:"#/opzioni",
-        title:"Opzioni"
+        url:"#/atleti",
+        title:"Atleti"
        }
     ];
-    vm.ver = swInfo.version;
-    vm.today = new Date();
-	vm.maximizeContainer=function(id){
-		var gc=document.getElementById(id);
-		if (!gc) return;
-		var t=gc.getBoundingClientRect().top;
-		var h = window.innerHeight;
-		if (h<200) h=200;
-		gc.style.height=h-t-20;
-	};
-}]);
+});
